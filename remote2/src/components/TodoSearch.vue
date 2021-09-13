e63ee5b989b67973be8006c08f5355852b1fc8e2
daikon128 <ndaichi06@gmail.com>
2021-09-14 03:02:27 +0900

    検索実装

--- remote2/src/components/TodoSearch.vue
+++ remote2/src/components/TodoSearch.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-    <input type="text"/>
-  </div>
+  <form>
+    <input v-model="searchText" type="text"/> <button v-on:input="search">search</button>
+  </form>
 </template>
 
 <script>
 export default {
-name: "TodoSearch"
+  name: "TodoSearch",
+  data() {
+    return {
+      searchText: ""
+    }
+  },
+  methods: {
+    search: function () {
+      console.log(`search: ${this.searchText}`)
+      return { todoLists: [
+          { "id": 1, "title": "買い物をする", "description": "カレーの具材"},
+          { "id": 2, "title": "本を読む", "description": "吾輩は猫である"},
+          { "id": 3, "title": "昼寝をする", "description": "30分"}
+        ]}
+
+    }
+  }
+
+
 }
 </script>
 
