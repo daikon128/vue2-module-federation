0ba50bc6d0fad63fbad5729e19171394b9ddbde0
daikon128 <ndaichi06@gmail.com>
2021-09-14 01:48:12 +0900

    :recycle:

--- remote2/src/main.js
+++ remote2/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Sandbox from './sandbox/Sandbox.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Sandbox),
 }).$mount('#app')
--- remote2/src/sandbox/Sandbox.vue
+++ remote2/src/sandbox/Sandbox.vue
@@ -7,12 +7,12 @@
 </template>
 
 <script>
-import CustomHeader from './components/CustomHeader.vue'
+import CustomHeader from '@/components/CustomHeader.vue'
 import TodoSearch from "@/components/TodoSearch";
 import TodoList from "@/components/TodoList";
 
 export default {
-  name: 'App',
+  name: 'Sandbox',
   components: {
     CustomHeader,
     TodoList,
