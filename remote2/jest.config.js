cf3dbc00047bbe3bf538890b97fed124b15c147c
daikon128 <ndaichi06@gmail.com>
2021-09-20 02:32:29 +0900

    jestの設定

--- remote2/jest.config.js
+++ remote2/jest.config.js
@@ -3,13 +3,14 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
   transform: {
     "^.+\\.vue$": "vue-jest",
-    "^.+\\.jsx?$": "babel-jest"
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
   },
   transformIgnorePatterns: ["node_modules/"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/js/$1"
+    "^@/(.*)$": "<rootDir>/src/$1"
   },
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ]
+
 }
