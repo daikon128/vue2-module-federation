e13f2dcbfe35fe1f66da5c994b8641c19425f182
daikon128 <ndaichi06@gmail.com>
2021-09-21 12:03:59 +0900

    todoSearchのテスト追加

--- remote2/tests/unit/components/TodoSearch.spec.js
+++ remote2/tests/unit/components/TodoSearch.spec.js
@@ -26,4 +26,23 @@ describe('TodoSearch', () => {
         expect(spy).to.be.calledOnce.calledWith(todoList)
       })
   })
+
+  it('検索文字列を入れた状態で検索ボタンを押下した際に、絞り込まれたTodoListが返却されること', () => {
+    const spy = cy.spy()
+    const wrapper = createWrapper().then(() => {
+      Cypress.vue.$on('search-todo-list', spy)
+    })
+
+    wrapper
+      .get('[name="todo-search-text-box"]')
+      .type('昼')
+      .get('[name="todo-search-button"]')
+      .click()
+      .then(() => {
+        const todoList = [
+          { "id": 3, "title": "昼寝をする", "description": "30分"}
+        ]
+        expect(spy).to.be.calledOnce.calledWith(todoList)
+      })
+  })
 })
