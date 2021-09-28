9a34a894afcb458f247ca2f4c0ebd5d49838545c
daikon128 <ndaichi06@gmail.com>
2021-09-29 02:07:10 +0900

    お試し:テストの順番を変えてみた

--- tests/cypress/integration/TodoPageIntegration.spec.js
+++ tests/cypress/integration/TodoPageIntegration.spec.js
@@ -3,12 +3,6 @@ describe('local integration', () => {
     cy.visit('/')
   })
 
-  it('ユーザ１がログインした状態の場合、広告に車が表示されること', ()=> {
-    cy.get('[name="big-adsense"]')
-      .get('[name="user-optimal-adsense"]')
-      .should('have.text', '車')
-  })
-
   it('絞り込み検索した際に特定のTodoListが表示されること', () => {
     cy.get('[name="todo-search-text-box"]')
       .type('本')
@@ -21,5 +15,11 @@ describe('local integration', () => {
       .children()
       .first()
       .should('have.text', '本を読む : 吾輩は猫である')
+  })
+
+  it('ユーザ１がログインした状態の場合、広告に車が表示されること', ()=> {
+    cy.get('[name="big-adsense"]')
+      .get('[name="user-optimal-adsense"]')
+      .should('have.text', '車')
   })
 })
