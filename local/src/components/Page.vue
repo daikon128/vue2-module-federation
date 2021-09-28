72ee618399820b8f3262fc2a2f4e6aa14216744f
daikon128 <ndaichi06@gmail.com>
2021-09-28 23:50:31 +0900

    サンプルとして不要なためHeaderを削除

--- local/src/components/Page.vue
+++ local/src/components/Page.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
-    <custom-header></custom-header>
     <todo-list :todoList="todoList"></todo-list>
     <big-adsense></big-adsense>
   </div>
 </template>
 
 <script>
-import CustomHeader from "@/components/CustomHeader"
-
 export default {
   name: "Page",
-  components: {
-    CustomHeader
-  },
   data() {
     return {
       todoList:[]
--- local/tests/cypress/integration/LocalIntegration.spec.js
+++ local/tests/cypress/integration/LocalIntegration.spec.js
@@ -2,11 +2,6 @@ describe('local integration', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('トップ画面でヘッダーが表示されること', ()=> {
-    cy.get('[name="custom-header"]')
-      .should('exist')
-  })
-
   it('トップ画面でTodoListが表示されること', ()=> {
     cy.get('[name="todo-list"]')
       .should('exist')
@@ -14,13 +9,6 @@ describe('local integration', () => {
 
   it('トップ画面で広告が表示されること', ()=> {
     cy.get('[name="big-adsense"]')
-      .should('exist')
-  })
-
-  it('設定リンクを押した際に設定に遷移されること', () => {
-    cy.get('[name="header-link-settings"]')
-      .click()
-      .get('[name="settings"]')
       .should('exist')
   })
 })
