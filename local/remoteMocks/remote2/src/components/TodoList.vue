84347390a9789e53e192e06c4d56dd515fd60ff0
daikon128 <ndaichi06@gmail.com>
2021-09-28 12:17:18 +0900

    remote2のモックを作成

---
+++ local/remoteMocks/remote2/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>TodoList dummy</div>
+</template>
+
+<script>
+export default {
+name: "TodoList"
+}
+</script>
+
+<style scoped>
+body {
+  background: repeating-linear-gradient(
+      45deg,
+      #ffffff,
+      #ffffff 28.75px,
+      #dddddd 28.75px,
+      #dddddd 57.5px
+  );
+  display: flex;
+  padding: 0.5rem;
+  justify-content: center;
+  align-items: center;
+}
+div {
+  display: block;
+  background: #fffe;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+}
+</style>
---
+++ local/remoteMocks/remote2/vue.config.js
@@ -0,0 +1,43 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+    },
+  },
+  devServer: {
+    port:8899
+  },
+  filenameHashing: true,
+  publicPath: "http://localhost:8899/",
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ]
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+      }
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'remote2',
+        library: {
+          type: 'var',
+          name: 'remote2'
+        },
+        filename: 'remote2.js',
+        exposes: {
+          "./TodoList": "./src/components/TodoList"
+        },
+      }),
+    ],
+  }
+}
